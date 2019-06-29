1f3086245bb8b4d15f4ce15b2f73bf9d9a4eddcb
Sara McMullin <mcsara1221@gmail.com>
2019-06-29 15:01:51 -0500

    updated liri.js

--- liri.js
+++ liri.js
@@ -7,3 +7,10 @@ var spotify = new Spotify(keys.spotify);
 //     secret: SPOTIFY_SECRET=your-spotify-secret
 // })
 
+// concert-this
+// spotify-this-song
+// movie-this
+// do-what-it-says
+
+var action = process.argv[2]
+var something = process.argv.splice(3).join(" ")
